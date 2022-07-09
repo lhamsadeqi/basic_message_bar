d2216ba2c1245019920e72a053077a31ada53c47
Mehrzad <mohammadi@dinawin.com>
2022-07-09 12:33:30 +0430

    font tested

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -38,6 +38,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        fontFamily: 'IRANSans',
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
